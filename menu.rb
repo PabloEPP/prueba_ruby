50ae7cd25b47b3842e26bb330120960f7324605d
pablo <pabloperezpastene@yahoo.es>
2019-05-14 14:34:29 -0400

    Se agregaron cambios a los metodos :(

--- menu.rb
+++ menu.rb
@@ -16,49 +16,44 @@ def calcular_promedio
       item_line.each_with_index do |elem, index|
         acc += elem.to_i / elem.to_f if index != 0
       end
-      puts "El Alumno #{item_line[0]} tiene Promedio #{acc}"
-      aux_arr[item_line[0]] = acc.
-    end
-    return aux_arr
-  end
-
-
-  #Imprimir en pantalla el nombre de cada alumno y la cantidad de inasistencias
-
-def inasistencias_alumno
-    aux_arr = {}
-    alum = read_alum('alumnos.csv')
-    alum.each do |item_line|
-      acc = 0
-      item_line.each_with_index do |elem, index|
-        acc += elem.to_i / elem.to_f if index != 0
-        puts "El Alumno #{item_line[0]} tiene #{acc} Inasistencias"
+        puts "El Alumno #{item_line[0]} tiene Promedio #{acc}"
         aux_arr[item_line[0]] = acc.
         end
-      return aux_arr
-  end
+        return aux_arr
+    end
+
+
+#Mostrar inasistencias
+
+def mostrar_inasistencias(alum)
+    alum.each_with_index do |elem, index|
+        puts "El elemento #{elem}, tiene indice #{index}"
+    end
+end
 
 
 
 
 
 
-
-  #Menu
+#Menu
 condition = 0
 while condition != 4
   puts "Elije una opción entre 1 y 3. 4 para salir"
-  condition = gets.chomp.to_i
-  case condition
-  when 1
-    calcular_promedio
-  when 2
-    inasistencias_alumno
-  when 3
-    #Metodo alumnos aprobados
-  when 4
-    puts "Adios"
-  else
-    puts "Opción no válida, ingrese una opcion del 1 al 3, 4 para Salir"
-  end
+    condition = gets.chomp.to_i
+    case condition
+        when 1
+            calcular_promedio
+        when 2
+            inasistencias_alumno
+        when 3
+            #Metodo alumnos aprobados
+        when 4
+            puts "Adios"
+        else
+            puts "Opción no válida, ingrese una opcion del 1 al 3, 4 para Salir"
+            
+        end
+    end
 end
+
