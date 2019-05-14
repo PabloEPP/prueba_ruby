9047b7398b86a088e7367392e6a4e540ed4882c0
pablo <pabloperezpastene@yahoo.es>
2019-05-14 13:44:10 -0400

    Se agregó el metodo inasistencias_alumno al menu.rb

--- menu.rb
+++ menu.rb
@@ -21,3 +21,19 @@ def calcular_promedio
     end
     return aux_arr
   end
+
+
+  #Imprimir en pantalla el nombre de cada alumno y la cantidad de inasistencias
+
+def inasistencias_alumno
+    aux_arr = {}
+    alum = read_alum('alumnos.csv')
+    alum.each do |item_line|
+      acc = 0
+      item_line.each_with_index do |elem, index|
+        acc += elem.to_i / elem.to_f if index != 0
+        puts "El Alumno #{item_line[0]} tiene #{acc} Inasistencias"
+        aux_arr[item_line[0]] = acc.
+        end
+      return aux_arr
+  end
