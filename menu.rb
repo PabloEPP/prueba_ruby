61165c31ed51066dbc4a5daf5b2fffae96c31a14
pablo <pabloperezpastene@yahoo.es>
2019-05-14 13:43:13 -0400

    Se agregó el metodo calcular_promedio al menu.rb

--- menu.rb
+++ menu.rb
@@ -4,3 +4,20 @@ def read_alum(file_name)
     file.close
     alum
 end
+
+
+#Imprimir en pantalla el nombre de cada alumno y el promedio de sus notas
+
+def calcular_promedio
+    aux_arr = {}
+    alum = read_alum('alumnos.csv')
+    alum.each do |item_line|
+      acc = 0
+      item_line.each_with_index do |elem, index|
+        acc += elem.to_i / elem.to_f if index != 0
+      end
+      puts "El Alumno #{item_line[0]} tiene Promedio #{acc}"
+      aux_arr[item_line[0]] = acc.
+    end
+    return aux_arr
+  end
